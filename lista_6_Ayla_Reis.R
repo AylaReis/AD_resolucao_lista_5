95665bfdca81608a414c3804f2a72c5fd1a717bb
Ayla Reis <ayla.reis810@gmail.com>
2019-05-18 21:03:35 -0300

    Resolução do exercício 1

---
+++ lista_6_Ayla_Reis.R
@@ -0,0 +1,17 @@
+######### Resolução da lista 6 - Análise de dados #######
+######### Aluna: Ayla Reis de Lima  ###################
+
+
+#### Exercício 1: Descreva os conceitos abaixo: ######
+
+### a) Variável dependente ###
+## A variável dependente, ou o valor de "Y" das equações matemáticas, se refere a uma grandeza 
+## em que seu valor irá depender de outra grandeza. ##
+
+### b) Variável independente ###
+## A variável independente, ou o valor de "X" das equações matemáticas, se refere a uma gran- 
+## deza em que seu valor irá não de outra grandeza para existir. ##
+
+### c) Apresente qual a relação existente entre variáveis independentes e dependente ###
+## A relação é de que o valor da várivel dependente está vulnerável aos valores das variáveis
+## independentes, ou seja, os "X"s  influenciará no valor de "Y". ## 
